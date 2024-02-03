611f58a3775d21f46ad627f20bd3f8e5971fb4eb
ryuga123677 <kishanss311@gmail.com>
2024-02-04 01:42:08 +0530

    changed nav colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <>
-    <div className='flex bg-white space-x-20 justify-end p-3'>
+    <div className='flex bg-white space-x-20 justify-end p-3 mb-2'>
         <div>About</div>
         <div>Home</div>
         <div>Contact us</div>
