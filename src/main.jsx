8c701fd59f494d80e0da544dd299292f17567c60
ryuga123677 <kishanss311@gmail.com>
2024-10-28 02:31:56 +0530

    env

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import dotenv from "dotenv";
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from "./pages/AuthContext.jsx"; 
+dotenv.config({
+  path:'../env'
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
  
   <BrowserRouter>
